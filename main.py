c6870c5f98bfee3dc57d0f79ee95b4a54d6fdbc8
Jayvee N Mapote <101295973+JvMapote@users.noreply.github.com>
2022-05-20 22:59:29 +0000

    commit

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+
+''' 
+Jayvee N Mapote
+2020161610
+CS104L/A12 
+
+Write a Python program that will generate the adjacency matrix representation of the
+weighted graph below:
+'''
+
+# Creating adjacency matrix manually
 def manual():
  arr = [[0 for i in range(5)] for j in range(5)]
  arr[0][1] = arr[1][0] = 2
@@ -7,6 +18,8 @@ def manual():
  arr[2][4] = arr[4][2] = 13
  arr[3][4] = arr[4][2] = 9
  return arr
+
+# getting user input to create adjacency matrix
 def userInput():
  arr = [[0 for i in range(5)] for j in range(5)]
  edges = int(input("Enter no. of edges: "))

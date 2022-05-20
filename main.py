f16b1de5a71c1cbcaa78a92997c9c070bc5f80c9
Jayvee N Mapote <101295973+JvMapote@users.noreply.github.com>
2022-05-20 11:31:30 +0000

    commit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def manual():
- # here created adjacency matrix manually
  arr = [[0 for i in range(5)] for j in range(5)]
  arr[0][1] = arr[1][0] = 2
  arr[0][2] = arr[2][0] = 3
@@ -9,7 +8,6 @@ def manual():
  arr[3][4] = arr[4][2] = 9
  return arr
 def userInput():
- # get user input to create adjacency matrxi
  arr = [[0 for i in range(5)] for j in range(5)]
  edges = int(input("Enter no. of edges: "))
  for i in range(edges):
